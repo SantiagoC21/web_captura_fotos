7e9bc33d1f5f54dd540be12e7cfeae90dcf675fa
SantiagoC21 <167811420+SantiagoC21@users.noreply.github.com>
2026-09-10 16:42:32 -0500

    cambios package.json 2

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 // src/api/client.ts
 import axios from 'axios'
 
-const BASE_URL = 'http://192.168.56.1:8000' // ajusta a tu backend
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
